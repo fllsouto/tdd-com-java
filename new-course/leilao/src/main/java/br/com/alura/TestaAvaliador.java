009f6b5af5cadd239ca33e52bef93d284f82df04
Fellipe Souto <fllsouto@gmail.com>
2020-11-10 09:53:52 -0300

    Aula 2 - testes redundantes

--- new-course/leilao/src/main/java/br/com/alura/TestaAvaliador.java
+++ new-course/leilao/src/main/java/br/com/alura/TestaAvaliador.java
@@ -11,6 +11,8 @@ import br.com.alura.dominio.Usuario;
 import br.com.alura.servico.Avaliador;
 
 public class TestaAvaliador {
+	
+	// Variação 1
 
 	@Test
 	public void deveAvaliarLancesEmOrdemCrescente() {
@@ -35,4 +37,202 @@ public class TestaAvaliador {
 
 		Assertions.assertEquals(menorEsperado, avaliador.getMenorLance());
 	}
+	
+	@Test
+	public void deveAvaliarLancesEmOrdemDecrescente() {
+		Usuario joao = new Usuario("João");
+		Usuario paulo = new Usuario("Paulo");
+		Usuario maria = new Usuario("Maria");
+
+		Leilao leilao = new Leilao("Playstation 4 Novo");
+
+		leilao.propoe(new Lance(maria, BigDecimal.valueOf(3000.00)));
+		leilao.propoe(new Lance(paulo, BigDecimal.valueOf(2000.00)));
+		leilao.propoe(new Lance(joao, BigDecimal.valueOf(1000.00)));
+
+		Avaliador avaliador = new Avaliador();
+		avaliador.avalia(leilao);
+
+
+		BigDecimal menorEsperado = BigDecimal.valueOf(1000.00);
+		BigDecimal maiorEsperado = BigDecimal.valueOf(3000.00);
+
+		Assertions.assertEquals(maiorEsperado, avaliador.getMaiorLance());
+
+		Assertions.assertEquals(menorEsperado, avaliador.getMenorLance());
+	}
+	
+	@Test
+	public void deveAvaliarLancesEmbaralhado() {
+		Usuario joao = new Usuario("João");
+		Usuario paulo = new Usuario("Paulo");
+		Usuario maria = new Usuario("Maria");
+
+		Leilao leilao = new Leilao("Playstation 4 Novo");
+
+		leilao.propoe(new Lance(paulo, BigDecimal.valueOf(2000.00)));
+		leilao.propoe(new Lance(joao, BigDecimal.valueOf(1000.00)));
+		leilao.propoe(new Lance(maria, BigDecimal.valueOf(3000.00)));
+
+		Avaliador avaliador = new Avaliador();
+		avaliador.avalia(leilao);
+
+
+		BigDecimal menorEsperado = BigDecimal.valueOf(1000.00);
+		BigDecimal maiorEsperado = BigDecimal.valueOf(3000.00);
+
+		Assertions.assertEquals(maiorEsperado, avaliador.getMaiorLance());
+
+		Assertions.assertEquals(menorEsperado, avaliador.getMenorLance());
+	}
+	
+	// Variação 2
+	
+	
+//	@Test
+//	public void deveAvaliarLancesEmOrdemCrescente2() {
+//		Usuario joao = new Usuario("João");
+//		Usuario paulo = new Usuario("Paulo");
+//		Usuario maria = new Usuario("Maria");
+//
+//		Leilao leilao = new Leilao("Playstation 4 Novo");
+//
+//		leilao.propoe(new Lance(joao, BigDecimal.valueOf(5000.00)));
+//		leilao.propoe(new Lance(paulo, BigDecimal.valueOf(7000.00)));
+//		leilao.propoe(new Lance(maria, BigDecimal.valueOf(9000.00)));
+//
+//		Avaliador avaliador = new Avaliador();
+//		avaliador.avalia(leilao);
+//
+//
+//		BigDecimal menorEsperado = BigDecimal.valueOf(5000.00);
+//		BigDecimal maiorEsperado = BigDecimal.valueOf(9000.00);
+//
+//		Assertions.assertEquals(maiorEsperado, avaliador.getMaiorLance());
+//
+//		Assertions.assertEquals(menorEsperado, avaliador.getMenorLance());
+//	}
+//	
+//	@Test
+//	public void deveAvaliarLancesEmOrdemDecrescente2() {
+//		Usuario joao = new Usuario("João");
+//		Usuario paulo = new Usuario("Paulo");
+//		Usuario maria = new Usuario("Maria");
+//
+//		Leilao leilao = new Leilao("Playstation 4 Novo");
+//
+//		leilao.propoe(new Lance(maria, BigDecimal.valueOf(9000.00)));
+//		leilao.propoe(new Lance(paulo, BigDecimal.valueOf(7000.00)));
+//		leilao.propoe(new Lance(joao, BigDecimal.valueOf(5000.00)));
+//
+//		Avaliador avaliador = new Avaliador();
+//		avaliador.avalia(leilao);
+//
+//
+//		BigDecimal menorEsperado = BigDecimal.valueOf(5000.00);
+//		BigDecimal maiorEsperado = BigDecimal.valueOf(9000.00);
+//
+//		Assertions.assertEquals(maiorEsperado, avaliador.getMaiorLance());
+//
+//		Assertions.assertEquals(menorEsperado, avaliador.getMenorLance());
+//	}
+//	
+//	@Test
+//	public void deveAvaliarLancesEmOrdemEmbaralhado2() {
+//		Usuario joao = new Usuario("João");
+//		Usuario paulo = new Usuario("Paulo");
+//		Usuario maria = new Usuario("Maria");
+//
+//		Leilao leilao = new Leilao("Playstation 4 Novo");
+//
+//		leilao.propoe(new Lance(maria, BigDecimal.valueOf(9000.00)));
+//		leilao.propoe(new Lance(joao, BigDecimal.valueOf(5000.00)));
+//		leilao.propoe(new Lance(paulo, BigDecimal.valueOf(7000.00)));
+//
+//		Avaliador avaliador = new Avaliador();
+//		avaliador.avalia(leilao);
+//
+//
+//		BigDecimal menorEsperado = BigDecimal.valueOf(5000.00);
+//		BigDecimal maiorEsperado = BigDecimal.valueOf(9000.00);
+//
+//		Assertions.assertEquals(maiorEsperado, avaliador.getMaiorLance());
+//
+//		Assertions.assertEquals(menorEsperado, avaliador.getMenorLance());
+//	}
+	
+	// Variação 3
+	
+//	@Test
+//	public void deveAvaliarLancesEmOrdemCrescente3() {
+//		Usuario joao = new Usuario("João");
+//		Usuario paulo = new Usuario("Paulo");
+//		Usuario maria = new Usuario("Maria");
+//
+//		Leilao leilao = new Leilao("Playstation 4 Novo");
+//
+//		leilao.propoe(new Lance(joao, BigDecimal.valueOf(50.00)));
+//		leilao.propoe(new Lance(paulo, BigDecimal.valueOf(100.00)));
+//		leilao.propoe(new Lance(maria, BigDecimal.valueOf(150.00)));
+//
+//		Avaliador avaliador = new Avaliador();
+//		avaliador.avalia(leilao);
+//
+//
+//		BigDecimal menorEsperado = BigDecimal.valueOf(50.00);
+//		BigDecimal maiorEsperado = BigDecimal.valueOf(150.00);
+//
+//		Assertions.assertEquals(maiorEsperado, avaliador.getMaiorLance());
+//
+//		Assertions.assertEquals(menorEsperado, avaliador.getMenorLance());
+//	}
+//	
+//	@Test
+//	public void deveAvaliarLancesEmOrdemDecrescente3() {
+//		Usuario joao = new Usuario("João");
+//		Usuario paulo = new Usuario("Paulo");
+//		Usuario maria = new Usuario("Maria");
+//
+//		Leilao leilao = new Leilao("Playstation 4 Novo");
+//
+//		leilao.propoe(new Lance(maria, BigDecimal.valueOf(150.00)));
+//		leilao.propoe(new Lance(paulo, BigDecimal.valueOf(100.00)));
+//		leilao.propoe(new Lance(joao, BigDecimal.valueOf(50.00)));
+//
+//		Avaliador avaliador = new Avaliador();
+//		avaliador.avalia(leilao);
+//
+//
+//		BigDecimal menorEsperado = BigDecimal.valueOf(50.00);
+//		BigDecimal maiorEsperado = BigDecimal.valueOf(150.00);
+//
+//		Assertions.assertEquals(maiorEsperado, avaliador.getMaiorLance());
+//
+//		Assertions.assertEquals(menorEsperado, avaliador.getMenorLance());
+//	}
+//	
+//	@Test
+//	public void deveAvaliarLancesEmOrdemEmbaralhado3() {
+//		Usuario joao = new Usuario("João");
+//		Usuario paulo = new Usuario("Paulo");
+//		Usuario maria = new Usuario("Maria");
+//
+//		Leilao leilao = new Leilao("Playstation 4 Novo");
+//
+//		leilao.propoe(new Lance(paulo, BigDecimal.valueOf(100.00)));
+//		leilao.propoe(new Lance(maria, BigDecimal.valueOf(150.00)));
+//		leilao.propoe(new Lance(joao, BigDecimal.valueOf(50.00)));
+//
+//		Avaliador avaliador = new Avaliador();
+//		avaliador.avalia(leilao);
+//
+//
+//		BigDecimal menorEsperado = BigDecimal.valueOf(50.00);
+//		BigDecimal maiorEsperado = BigDecimal.valueOf(150.00);
+//
+//		Assertions.assertEquals(maiorEsperado, avaliador.getMaiorLance());
+//
+//		Assertions.assertEquals(menorEsperado, avaliador.getMenorLance());
+//	}
+
 }
