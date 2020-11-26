c0296a45f62053a87d7d83c17e044bd1f301bd8f
Fellipe Souto <fllsouto@gmail.com>
2020-11-25 22:17:12 -0300

    Aula 3 - adicionando testes para a classe Leilao

--- new-course/leilao/src/main/java/br/com/alura/TestaLeilao.java
+++ new-course/leilao/src/main/java/br/com/alura/TestaLeilao.java
@@ -2,23 +2,46 @@ package br.com.alura;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
 import br.com.alura.dominio.Lance;
 import br.com.alura.dominio.Leilao;
 import br.com.alura.dominio.Usuario;
 
 public class TestaLeilao {
 
-	public static void main(String[] args) {
+	@Test
+	public void deveReceberUmLance() {
 		Usuario joao = new Usuario("João");
-		Usuario paulo = new Usuario("Paulo");
-		Usuario maria = new Usuario("Maria");
 
 		Leilao leilao = new Leilao("Playstation 4 Novo");
 
 		leilao.propoe(new Lance(joao, BigDecimal.valueOf(1000.00)));
-		leilao.propoe(new Lance(paulo, BigDecimal.valueOf(1200.00)));
-		leilao.propoe(new Lance(maria, BigDecimal.valueOf(1400.00)));
-		
-		System.out.println("Total de lances: " + leilao.getLances().size());
+
+		int quantidadeDeLancesEsperado = 1;
+		BigDecimal valorDoLanceEsperado = BigDecimal.valueOf(1000.00);
+
+		assertEquals(quantidadeDeLancesEsperado, leilao.getLances().size());
+		assertEquals(valorDoLanceEsperado, leilao.getLances().get(0).getValor());
+	}
+
+	@Test
+	public void deveReceberVariosLances() {
+		Usuario joao = new Usuario("João");
+		Usuario paulo = new Usuario("Paulo");
+
+		Leilao leilao = new Leilao("Playstation 4 Novo");
+
+		leilao.propoe(new Lance(joao, BigDecimal.valueOf(1000.00)));
+		leilao.propoe(new Lance(paulo, BigDecimal.valueOf(2000.00)));
+
+		int quantidadeDeLancesEsperado = 2;
+		BigDecimal valorDoPrimeiroLanceEsperado = BigDecimal.valueOf(1000.00);
+		BigDecimal valorDoSegundoLanceEsperado = BigDecimal.valueOf(2000.00);
+
+		assertEquals(quantidadeDeLancesEsperado, leilao.getLances().size());
+		assertEquals(valorDoPrimeiroLanceEsperado, leilao.getLances().get(0).getValor());
+		assertEquals(valorDoSegundoLanceEsperado, leilao.getLances().get(1).getValor());
 	}
 }
