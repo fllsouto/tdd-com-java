8977400a935f2d091a88a99032cac05ad570a0d9
Fellipe Souto <fllsouto@gmail.com>
2020-12-15 11:21:50 -0300

    aula 4 - passando terceiro teste

--- new-course/leilao/src/main/java/br/com/alura/dominio/CalculadoraImposto.java
+++ new-course/leilao/src/main/java/br/com/alura/dominio/CalculadoraImposto.java
@@ -5,8 +5,12 @@ import java.math.BigDecimal;
 public class CalculadoraImposto {
 
 	public BigDecimal calculaImpostoParaLances(Leilao leilao) {
-		if (leilao.getLances().get(0).getValor().compareTo(BigDecimal.valueOf(1000.00)) == 1)
+		if (leilao.getLances().get(0).getValor().compareTo(BigDecimal.valueOf(1000.00)) == 1) {
+			if (leilao.getLances().get(0).getValor().compareTo(BigDecimal.valueOf(10000.00)) == 1)
+				return BigDecimal.valueOf(2250.00);
+
 			return BigDecimal.valueOf(450.00);
+		}
 
 		return BigDecimal.valueOf(15.00);
 	}
--- new-course/leilao/src/test/java/br/com/alura/TestaCalculadoraImposto.java
+++ new-course/leilao/src/test/java/br/com/alura/TestaCalculadoraImposto.java
@@ -11,9 +11,8 @@ import br.com.alura.dominio.Usuario;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 public class TestaCalculadoraImposto {
-	
+
 	@Test
 	public void deveCalcularImpostoDeEletronicosParaPrimeiraFaixa() {
 		Usuario joao = new Usuario("João");
@@ -25,16 +24,16 @@ public class TestaCalculadoraImposto {
 		leilao.propoe(new Lance(joao, BigDecimal.valueOf(500.00)));
 		leilao.propoe(new Lance(paulo, BigDecimal.valueOf(300.00)));
 		leilao.propoe(new Lance(maria, BigDecimal.valueOf(700.00)));
-		
+
 		CalculadoraImposto calculadora = new CalculadoraImposto();
-		
+
 		BigDecimal impostoCalculadoObtido = calculadora.calculaImpostoParaLances(leilao);
-		
+
 		BigDecimal impostoCalculadoEsperado = BigDecimal.valueOf(15.00);
-		
+
 		assertEquals(impostoCalculadoEsperado, impostoCalculadoObtido);
 	}
-	
+
 	@Test
 	public void deveCalcularImpostoDeEletronicosParaSegundaFaixa() {
 		Usuario joao = new Usuario("João");
@@ -46,13 +45,34 @@ public class TestaCalculadoraImposto {
 		leilao.propoe(new Lance(joao, BigDecimal.valueOf(5000.00)));
 		leilao.propoe(new Lance(paulo, BigDecimal.valueOf(3000.00)));
 		leilao.propoe(new Lance(maria, BigDecimal.valueOf(7000.00)));
-		
+
 		CalculadoraImposto calculadora = new CalculadoraImposto();
-		
+
 		BigDecimal impostoCalculadoObtido = calculadora.calculaImpostoParaLances(leilao);
-		
+
 		BigDecimal impostoCalculadoEsperado = BigDecimal.valueOf(450.00);
-		
+
+		assertEquals(impostoCalculadoEsperado, impostoCalculadoObtido);
+	}
+
+	@Test
+	public void deveCalcularImpostoDeEletronicosParaTerceiraFaixa() {
+		Usuario joao = new Usuario("João");
+		Usuario paulo = new Usuario("Paulo");
+		Usuario maria = new Usuario("Maria");
+
+		Leilao leilao = new Leilao("Macbook Pro");
+
+		leilao.propoe(new Lance(joao, BigDecimal.valueOf(15000.00)));
+		leilao.propoe(new Lance(paulo, BigDecimal.valueOf(13000.00)));
+		leilao.propoe(new Lance(maria, BigDecimal.valueOf(17000.00)));
+
+		CalculadoraImposto calculadora = new CalculadoraImposto();
+
+		BigDecimal impostoCalculadoObtido = calculadora.calculaImpostoParaLances(leilao);
+
+		BigDecimal impostoCalculadoEsperado = BigDecimal.valueOf(2250.00);
+
 		assertEquals(impostoCalculadoEsperado, impostoCalculadoObtido);
 	}
 
