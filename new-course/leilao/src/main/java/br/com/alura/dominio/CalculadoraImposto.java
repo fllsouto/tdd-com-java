e76594fbb1b7b05e4e765ab33610a7ba77939512
Fellipe Souto <fllsouto@gmail.com>
2020-12-15 11:06:56 -0300

    aula 4 - passando primeiro teste

--- new-course/leilao/src/main/java/br/com/alura/dominio/CalculadoraImposto.java
+++ new-course/leilao/src/main/java/br/com/alura/dominio/CalculadoraImposto.java
@@ -1,5 +1,11 @@
 package br.com.alura.dominio;
 
+import java.math.BigDecimal;
+
 public class CalculadoraImposto {
+
+	public BigDecimal calculaImpostoParaLances(Leilao leilao) {
+		return BigDecimal.valueOf(15.00);
+	}
 
 }
